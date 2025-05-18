51530f46f9401f4080115f4efc6a842f63e93d2f
sdsmnc221 <antr.2201@gmail.com>
2025-05-18 09:46:25 +0200

    test(app): maj-8 sw

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import "./assets/index.css";
 import App from "./App.vue";
+import { registerServiceWorker } from "./registerServiceWorker";
 
 createApp(App).mount("#app");
+
+registerServiceWorker();
